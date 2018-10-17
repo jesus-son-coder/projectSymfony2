f8ebe6494cda1d0f1856d6d7b414049c819a79c7
Herve Seka <sekaherve@gmail.com>
2018-10-17 13:41:39 +0200

    Création d'une méthode de la classe "LoadUserData" pour générer automatiquement des Users de tests en base de données.

--- src/TechCorp/FrontBundle/DataFixtures/ORM/LoadUserData.php
+++ src/TechCorp/FrontBundle/DataFixtures/ORM/LoadUserData.php
@@ -1,34 +1,58 @@
 <?php
 namespace TechCorp\FrontBundle\DataFixtures\ORM;
 
-use Doctrine\Common\DataFixtures\AbstractFixture;
-use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
+use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 use TechCorp\FrontBundle\Entity\User;
-use TechCorp\FrontBundle\Entity\Status;
+// use TechCorp\FrontBundle\Entity\Status;
 
-class LoadUserData extends AbstractFixture implements OrderedFixtureInterface
+class LoadUserData implements FixtureInterface, ContainerAwareInterface
 {
     const MAX_NB_USERS = 10;
+
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null)
+    {
+        $this->container = $container;
+    }
 
     public function load(ObjectManager $manager)
     {
         $faker = \Faker\Factory::create();
 
-        for($i=0; $i<self::MAX_NB_USERS; ++$i) {
-            // On crée un nouvel utilisateur
-            $user = new User();
-            $user->setUsername($faker->userName);
-            $manager->persist($user);
+        $userManager = $this->container->get('fos_user.user_manager');
 
-            $this->addReference('user'. $i, $user);
+        for($i=0; $i<self::MAX_NB_USERS; ++$i) {
+            // On crée un nouvel utilisateur aléatoire
+            $user = $userManager->createUser();
+            $user->setUsername($faker->userName);
+            $user->setEmail($faker->email);
+            $user->setPlainPassword($faker->password);
+            $user->setEnabled(true);
+
+            $manager->persist($user);
         }
+
+        // On crée un utilisateur dont on contrôle le nom et le mot de passe :
+        $user = $userManager->createUser();
+        $user->setUsername('user');
+        $user->setEmail($faker->email);
+        $user->setPlainPassword('password');
+        $user->setEnabled(true);
+
+        $manager->persist($user);
+
         $manager->flush();
     }
 
-    public function getOrder()
-    {
-        return 1;
-    }
 }
