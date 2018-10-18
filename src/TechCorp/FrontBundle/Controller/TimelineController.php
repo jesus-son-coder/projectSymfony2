7041c14025ef125abd9165c4006f3c95d8fbd03a
Herve Seka <sekaherve@gmail.com>
2018-10-19 00:02:13 +0200

    Création du formulaire de génération de Statuts (sur la timeline d'un utilisateur) + gestion de la soumission du formulaire

--- src/TechCorp/FrontBundle/Controller/TimelineController.php
+++ src/TechCorp/FrontBundle/Controller/TimelineController.php
@@ -4,6 +4,7 @@ namespace TechCorp\FrontBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use TechCorp\FrontBundle\Entity\Status;
+use TechCorp\FrontBundle\Form\StatusType;
 
 class TimelineController extends Controller
 {
@@ -16,15 +17,41 @@ class TimelineController extends Controller
 
     public function userTimelineAction($userId)
     {
+        // 1) Récurérer l'utilisateur :
         $em = $this->getDoctrine()->getManager();
         $user = $em->getRepository('TechCorpFrontBundle:User')->findOneById($userId);
         if(!$user) {
             $this->createNotFoundException("L'utilisateur n'a pas été trouvé.");
         }
+
+        // 2) Créer le Formulaire :
+        $authenticatedUser = $this->get('security.context')->getToken()->getUser();
+        $status = new Status();
+        $status->setDeleted(false);
+        $status->setUser($authenticatedUser);
+
+        $form = $this->createForm(new StatusType(), $status);
+        $request = $this->get('request_stack')->getCurrentRequest();
+
+        $form->handleRequest($request);
+
+        // 3) Traiter le formulaire :
+        if($authenticatedUser && $form->isValid()) {
+            $em->persist($status);
+            $em->flush();
+            $this->redirect($this->generateUrl('tech_corp_front_user_timeline', array(
+                'userId' => $authenticatedUser->getId()
+            )));
+        }
+
+        // 4) Récupérer les statuts :
         $statuses = $em->getRepository('TechCorpFrontBundle:Status')->getUserTimeline($user)->getResult();
+
+        // 5) Rendre la page :
         return $this->render('TechCorpFrontBundle:Timeline:user_timeline.html.twig', array(
             'user' => $user,
             'statuses' => $statuses,
+            'form' => $form->createView(),
         ));
     }
 
