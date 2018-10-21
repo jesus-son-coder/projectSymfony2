f5b26150f92e970104fe70088765eb7598c6015b
Herve Seka <sekaherve@gmail.com>
2018-10-21 03:50:05 +0200

    Installation du bundle DoctrineMigration

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new TechCorp\FrontBundle\TechCorpFrontBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
             new FOS\UserBundle\FOSUserBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
